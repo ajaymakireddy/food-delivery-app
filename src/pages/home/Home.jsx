144dd7055361b5bc89ca9faa3953e833eaa25d70
AJAY KUMAR <ajaymakireddy@gmail.com>
2025-10-29 10:14:20 +0530

    Completed Display Items

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ExploreMenu from "../../components/exploermenu/ExploreMenu";
 import Header from "../../components/header/Header";
 import "./Home.css";
+import FoodDisplay from "../../components/fooddisplay/FoodDisplay";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
@@ -11,6 +12,7 @@ const Home = () => {
       <div>
         <Header />
         <ExploreMenu category={category} setCategory={setCategory} />
+        <FoodDisplay />
       </div>
     </>
   );
